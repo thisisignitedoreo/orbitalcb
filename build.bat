1015a51cfb1f80258e6b7bc61dcd79fa151f070b
aciddev_ <a@a.com>
2024-07-12 18:08:01 +0300

    win build stuff

--- build.bat
+++ build.bat
@@ -6,3 +6,11 @@ pyside6-rcc main.qrc -o main_rc.py
 pyinstaller Orbital.spec
 nuitka --windows-icon-from-ico=assets\icon.ico cmd.py
 
+rd build /s/q
+rd cmd.build /s/q
+rd cmd.dist /s/q
+rd cmd.onefile-build /s/q
+del cmd.cmd
+move cmd.exe OrbitalCmd.exe
+move dist\Orbital.exe Orbital.exe
+rd dist /s/q
