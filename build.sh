ea7d54d7007bcf4ce0f2b7547e0755fa88e63248
aciddev_ <a@a.com>
2024-07-12 17:55:27 +0300

    build stuff

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -xe
+
+~/.local/bin/pyside6-uic main.ui -o ui_main.py
+~/.local/bin/pyside6-rcc main.qrc -o main_rc.py
+
+python -m PyInstaller Orbital.spec
+python -m nuitka --onefile cmd.py
+
+rm -r cmd.build cmd.dist build
+mv dist/Orbital ./
+mv cmd.bin OribtalCmd
+rm -r dist
